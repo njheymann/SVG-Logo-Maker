54ec23a1fc860a266e6c1e00ccf05735bc6b93e9
njheymann <nils.heymann@Hotmail.com>
2023-07-25 11:33:53 +1000

    Added necessary packages, added shapes and rendering, started tests

---
+++ index.js
@@ -0,0 +1,65 @@
+const fs = require("fs");
+const inquirer = require("inquirer");
+const Shape = require("./lib/shapes");
+
+
+const createSVGFile = (svgContent) => {
+  fs.writeFile("logo.svg", svgContent, (err) => {
+    if (err) throw err;
+    console.log("Generated logo.svg");
+  });
+};
+
+const generateSVG = (initials, textColor, logoShape, shapeColor) => {
+  let shape;
+  switch (logoShape) {
+    case "circle":
+      shape = new Shape.Circle();
+      break;
+    case "triangle":
+      shape = new Shape.Triangle();
+      break;
+    case "square":
+      shape = new Shape.Square();
+      break;
+  }
+
+  shape.shapeColour(shapeColor);
+  const renderedShape = shape.render();
+  const text = `<text x="50%" y="50%" text-anchor="middle" fill="${textColor}" font-size="50">${initials}</text>`;
+
+  return `<svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">${renderedShape}${text}</svg>`;
+};
+
+const main = async () => {
+  const userInput = await inquirer.prompt([
+    {
+      type: "input",
+      name: "initials",
+      message: "Enter up to three characters:",
+      validate: (input) => input.length <= 3,
+    },
+    {
+      type: "input",
+      name: "textColor",
+      message: "Enter the text color (color keyword or hexadecimal number):",
+    },
+    {
+      type: "list",
+      name: "logoShape",
+      message: "Choose a logo shape:",
+      choices: ["circle", "triangle", "square"],
+    },
+    {
+      type: "input",
+      name: "shapeColor",
+      message: "Enter the shape color (color keyword or hexadecimal number):",
+    },
+  ]);
+
+  const { initials, textColor, logoShape, shapeColor } = userInput;
+  const svgContent = generateSVG(initials, textColor, logoShape, shapeColor);
+  createSVGFile(svgContent);
+};
+
+main();
